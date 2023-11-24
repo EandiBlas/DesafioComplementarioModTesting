ca9476e35469d005e817d2e35427d811e48d9a5b
EandiBlas <eandiblas@gmail.com>
2023-11-23 23:56:14 -0300

    first commit

---
+++ src/persistencia/dao/models/ticket.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const ticketSchema = new mongoose.Schema({
+    code: {
+        type: String,
+        unique: true,
+        default: () => {
+            return Math.random().toString(36).substring(7);
+        },
+    },
+    purchase_datetime: {
+        type: Date,
+        default: Date.now,
+    },
+    amount: {
+        type: Number,
+        required: true,
+    },
+    purchaser: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+});
+export const ticketModel = mongoose.model('Ticket', ticketSchema);
+
